82bd012f1cb9c944b66f09b9872faf02674e9dc0
Clayton Landry <clayton.landry@gmail.com>
2021-06-16 11:55:58 -0400

    persisting reminder

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddTask from './components/AddTask'
 
 
 const App = () => {
-  const [showAddTask, setShowAddTask] = useState(true)
+  const [showAddTask, setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([])
 
@@ -26,22 +26,52 @@ const App = () => {
     return data
   }
 
+  const fetchTask = async (id) => {
+    const response = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await response.json()
+
+    return data
+  }
+
   // Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = {id, ...task}
+  const addTask = async (task) => {
+    const newTaskResponse = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+      'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const newTask = await newTaskResponse.json()
     setTasks([...tasks, newTask])
+
   }
 
   // Delete task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,
+    {
+      method: 'DELETE',
+    })
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   // Toggle reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToUpdate = await fetchTask(id)
+    const updatedTask = { ...taskToUpdate, reminder: !taskToUpdate.reminder }
+    const response = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updatedTask)
+    })
+    const data = await response.json()
+
     setTasks(
-      tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+      tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
   }
   return (
     <div className="container">
